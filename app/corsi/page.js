78bc89b625dd2a2b1f83aca92841e3c800a9c485
Ossama Tijani <nalothegreat0@gmail.com>
2024-05-28 11:38:34 +0200

    aggiornamento api pagina corsi

--- app/corsi/page.js
+++ app/corsi/page.js
@@ -1,110 +1,109 @@
 "use client";
 
 import classes from './page.module.css';
-
-import { useEffect } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import VR from '@/assets/VR.png';
-import data from '@/assets/analist.png';
-import webdev from '@/assets/webdev.png';
-import cloud from '@/assets/cloud.png';
-import mark from '@/assets/mark.png';
-import webdes from '@/assets/webdes.png';
-import Image from "next/image";
 
 import CourseCard from '@/components/card-component';
 
 export default function Corsi() {
   const router = useRouter();
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const checkAuthentication = async () => {
       try {
-        const response = await axios.get('/api/check-authentication'); 
-        const { authenticated, role } = response.data;
-        if (!authenticated || role !== 'student') {
-          router.push('/login');
+        const response = await fetch('http://localhost:8080/auth/profile', {
+          credentials: 'include' // Includiamo i cookie con la richiesta
+        });
+        if (response.ok) {
+          const data = await response.json();
+          const { ruolo } = data; // Assumendo che il profilo utente contenga il campo 'ruolo'
+          if (ruolo !== 's') {
+            router.push('/corsi');
+          }
+        } else {
+          router.push('/corsi');
         }
       } catch (error) {
         console.error('Errore durante il controllo dell\'autenticazione:', error);
+        router.push('/auth/login');
       }
     };
     checkAuthentication();
   }, [router]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/corsi', {
+          credentials: 'include'
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setCourses(data);
+          setLoading(false);
+        } else {
+          console.error('Errore durante il fetch dei corsi:', response.statusText);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error('Errore durante il fetch dei corsi:', error);
+        setLoading(false);
+      }
+    };
+    fetchCourses();
+  }, []);
+
   const applyCourse = async (courseId) => {
     try {
-      const response = await axios.post('/api/apply-course', { courseId });
-      console.log('Candidatura inviata con successo:', response.data);
+      const response = await fetch(`http://localhost:8080/candidatura/iscrizione/${courseId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include'
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Candidatura inviata con successo:', data);
+      } else {
+        console.error('Errore durante l\'invio della candidatura:', response.statusText);
+      }
     } catch (error) {
       console.error('Errore durante l\'invio della candidatura:', error);
     }
   };
 
-  const courses = [
-    {
-      id: 1,
-      title: "AR/VR Game Developer",
-      category: "Nuove Tecnologie",
-      instructor: "Nome Docente",
-      image: VR,
-      description: "Diventa AR/VR Game Developer: acquisirai le competenze per modellare, strutturare, sviluppare e pubblicare applicazioni per la realtà aumentata e virtuale, videogiochi sui principali store, e creare esperienze interattive nei nuovi Metaversi."
-    },
-    {
-      id: 2,
-      title: "Data Analysis",
-      category: "Programmazione",
-      instructor: "Nome Docente",
-      image: data,
-      description: "Diventa professionista della gestione e analisi dei dati: imparerai a mettere i dati al servizio del business e a trovare la tua opportunità professionale nelle migliori aziende in Italia e all’estero."
-    },
-    {
-      id: 3,
-      title: "Web Developer",
-      category: "Programmazione",
-      instructor: "Nome Docente",
-      image: webdev,
-      description: "Diventa Web Developer, avrai le competenze per supportare lo sviluppo e l’integrazione di soluzioni software evolute all’interno dei processi aziendali. Sarai in grado di lavorare con tecnologie moderne e contribuire all'innovazione delle aziende."
-    },
-    {
-      id: 4,
-      title: "Cloud Specialist",
-      category: "Nuove Tecnologie",
-      instructor: "Nome Docente",
-      image: cloud,
-      description: "Diventa professionista della creazione di ambienti digitali complessi: imparerai a gestire ogni fase della progettazione di software e potrai lavorare nelle migliori aziende in Italia e all’estero."
-    },
-    {
-      id: 5,
-      title: "Digital Marketing",
-      category: "Comunicazione",
-      instructor: "Nome Docente",
-      image: mark,
-      description: "Diventa un professionista capace di conoscere e creare strategie marketing per i new media: potrai trovare lavoro come Digital ADS Specialist, Social Media Manager, Copywriter e molto altro."
-    },
-    {
-      id: 6,
-      title: "Web Design",
-      category: "Comunicazione",
-      instructor: "Nome Docente",
-      image: webdes,
-      description: "Diventa un professionista e gestisci ogni aspetto della comunicazione digital delle aziende: potrai trovare lavoro come Web Designer, Graphic Designer, Video Editor e molto altro."
+  const groupedCourses = courses.reduce((acc, course) => {
+    if (!acc[course.category]) {
+      acc[course.category] = [];
     }
-  ];
+    acc[course.category].push(course);
+    return acc;
+  }, {});
+
+  if (loading) {
+    return <p>Caricamento...</p>;
+  }
 
   return (
     <div className={classes.corsi}>
-    <div  className="container mt-5">
-      
-      <h1 className="mb-4">Corsi disponibili</h1>
-      <div className="row">
-          {courses.map(course => (
-            <CourseCard key={course.id} course={course} applyCourse={applyCourse} />
-          ))}
+      <div className="container mt-5">
+        <h1 className="mb-4">Corsi disponibili</h1>
+        {Object.keys(groupedCourses).map(category => (
+          <div key={category}>
+            <h2>{category}</h2>
+            <div className="row">
+              {groupedCourses[category].map(course => (
+                <CourseCard key={course.id} course={course} applyCourse={applyCourse} />
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
     </div>
   );
 }
