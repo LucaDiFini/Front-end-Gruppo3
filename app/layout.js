b911b575d18ad9660c78d8a482f202aec3eafebd
Marcos Alejandro Maida Claros <marcos.maida@itsincom.it>
2024-05-29 12:42:04 +0200

    aggiunta di admin e ripristino di pagine modifcate

--- app/layout.js
+++ app/layout.js
@@ -15,11 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <MainHeader />
+        <MainHeader />
         {children}
-      
-      
-      <Footer />
+
+
+        <Footer />
       </body>
     </html>
   );
