15cc4f12f9a83655dec59d011f97e9d82cb133cc
Ossama Tijani <nalothegreat0@gmail.com>
2024-05-29 13:28:54 +0200

    correzione admin-corsi col merge

--- app/corsi/page.js
+++ app/corsi/page.js
@@ -1,16 +1,16 @@
 "use client";
-
+ 
 import classes from './page.module.css';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CourseCard from '@/components/card-component';
-
+ 
 export default function Corsi() {
   const router = useRouter();
   const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(true);
-
+ 
   useEffect(() => {
     const checkAuthentication = async () => {
       try {
@@ -33,7 +33,7 @@ export default function Corsi() {
     };
     checkAuthentication();
   }, [router]);
-
+ 
   useEffect(() => {
     const fetchCourses = async () => {
       try {
@@ -62,7 +62,7 @@ export default function Corsi() {
     };
     fetchCourses();
   }, []);
-
+ 
   const applyCourse = async (courseId) => {
     try {
       const response = await fetch(`http://localhost:8080/candidatura/iscrizione/${courseId}`, {
@@ -82,7 +82,7 @@ export default function Corsi() {
       console.error('Errore durante l\'invio della candidatura:', error);
     }
   };
-
+ 
   const groupedCourses = courses.reduce((acc, course) => {
     if (!acc[course.category]) {
       acc[course.category] = [];
@@ -90,11 +90,11 @@ export default function Corsi() {
     acc[course.category].push(course);
     return acc;
   }, {});
-
+ 
   if (loading) {
     return <p>Caricamento...</p>;
   }
-
+ 
   return (
     <div className={classes.corsi}>
       <div className="container mt-5">
--- app/login/page.js
+++ app/login/page.js
@@ -1,21 +1,21 @@
 "use client";
-
+ 
 import React, { useState } from 'react';
 import { loginUser } from '../../utils/api';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './page.module.css';
 import InputForm from '@/components/input_form';
 import useNavigation from '../../utils/useNavigation';
-
+ 
 export default function Pagina_di_Accesso() {
   const navigateTo = useNavigation();
   const [form, setForm] = useState({
     email: '',
     password: '',
   });
-
+ 
   const [error, setError] = useState(null);
-
+ 
   const handleChange = (e) => {
     const { id, value } = e.target;
     setForm((prevForm) => ({
@@ -23,7 +23,7 @@ export default function Pagina_di_Accesso() {
       [id]: value,
     }));
   };
-
+ 
   const handleLogin = async (e) => {
     e.preventDefault();
     setError(null);
@@ -34,22 +34,22 @@ export default function Pagina_di_Accesso() {
       };
       const response = await loginUser(userData);
       console.log('User accessed:', response);
-
+ 
       // SERVIRA' PER SPOSTARE L'UTENTE SE IL LOGIN E' ANDATO A BUON FINE
       //router.push('/dashboard'); // Redirect to the dashboard page
-
+ 
       const currentPage = window.location.pathname;
       if (currentPage === '/login') {
         navigateTo('/');
       } else {
         navigateTo('/corsi');
       }
-
+ 
     } catch (err) {
       setError(err.message || 'login failed');
     }
   };
-
+ 
   return (
     <div className="bg-body-secondary">
       <div className={`modal modal-sheet position-static d-block p-4 py-md-5 ${styles.login}`} tabIndex="-1" role="dialog" id="modalSignin">
@@ -59,7 +59,7 @@ export default function Pagina_di_Accesso() {
               <h2 className="fw-bold mb-0">Bentornato!</h2>
               <p>Accedi per scoprire le ultime novità e continuare a imparare con noi.</p>
             </div>
-
+ 
             <div className={`modal-content rounded-4 shadow ${styles['form-bg-azzurro']} col-lg-6 p-4`}>
               <div className="modal-header p-5 pb-4 border-bottom-0">
                 <h1 className="fw-bold mb-0 fs-2">Login</h1>
@@ -74,7 +74,7 @@ export default function Pagina_di_Accesso() {
                   >
                     Indirizzo email
                   </InputForm>
-
+ 
                   <InputForm
                     type="password"
                     id="password"
@@ -83,7 +83,7 @@ export default function Pagina_di_Accesso() {
                   >
                     Password
                   </InputForm>
-
+ 
                   <button className="d-block w-100 btn btn-danger mb-2 rounded-3" type="submit">Accedi</button>
                   <small className="text-body-secondary">Password dimenticata?</small>
                   <hr className="my-4" />
@@ -110,3 +110,4 @@ export default function Pagina_di_Accesso() {
     </div>
   );
 }
+ 
